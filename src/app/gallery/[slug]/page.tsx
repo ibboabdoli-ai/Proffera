446f9471aeaab8e9f7d97f358b8d59fb304eb449
Ibbo Abdoli <ibbo.abdoli@gmail.com>
2026-08-06 15:59:37 +0200

    fix: declare English gallery route config locally

--- src/app/gallery/[slug]/page.tsx
+++ src/app/gallery/[slug]/page.tsx
@@ -1,1 +1,5 @@
-export { dynamic, default } from "@/app/galleri/[slug]/page";
+import WorkspaceGalleryPage from "@/app/galleri/[slug]/page";
+
+export const dynamic = "force-dynamic";
+
+export default WorkspaceGalleryPage;
