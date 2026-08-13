12b9df7a04338390601fd7e59c5b569082777305
Ibbo Abdoli <ibbo.abdoli@gmail.com>
2026-08-13 16:56:19 +0200

    Add English quote route

---
+++ src/app/en/get-quote/page.tsx
@@ -0,0 +1,5 @@
+import { QuoteRequestForm } from "@/features/quote-request/quote-request-form";
+
+export default function EnglishQuotePage() {
+  return <main className="mx-auto max-w-5xl px-4 py-16"><QuoteRequestForm locale="en" /></main>;
+}
