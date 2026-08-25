44f1133e3fc722d728ad069a58980d0418240b69
Ibbo Abdoli <ibbo.abdoli@gmail.com>
2026-08-25 09:05:53 +0200

    Preserve local SEO filters in pagination

--- src/app/hitta/[service]/[location]/page.tsx
+++ src/app/hitta/[service]/[location]/page.tsx
@@ -59,7 +59,7 @@ export default async function DirectoryServiceLocationPage({ params }: Props) {
         </div>
       </section>
       <div className="mx-auto max-w-7xl px-4 pb-12 pt-6 sm:px-6 lg:px-8">
-        <PublicDirectoryResults locale="sv" search={search} />
+        <PublicDirectoryResults locale="sv" search={search} paginationBaseHref={searchHref} />
       </div>
     </main>
   );
